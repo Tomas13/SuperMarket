656c6f26ec0e3873e44d78218f0faf0e288cad51
Zhangali Bidaibekov <tomas.mapoter@gmail.com>
2017-07-28 19:52:45 +0600

    response code 200 success result

--- app/src/main/java/kazpost/kz/supermarket/ui/scanner/ScanPresenter.java
+++ app/src/main/java/kazpost/kz/supermarket/ui/scanner/ScanPresenter.java
@@ -62,25 +62,46 @@ public class ScanPresenter<V extends ScanMvpView> extends BasePresenter<V> imple
             public void onResponse(Call<Response> call, retrofit2.Response<Response> response) {
 
 
-                String value = null;
-                try {
-                    value = response.errorBody().string();
-                } catch (IOException e) {
-                    e.printStackTrace();
+                if (response.code() == 200) {
+                    /*String value = null;
+                    value = response.body().getMessage();
+                    value = value.substring(1, value.length() - 1);           //remove curly brackets
+                    String[] keyValuePairs = value.split(",");              //split the string to creat key-value pairs
+                    Map<String, String> map = new HashMap<>();
+
+                    for (String pair : keyValuePairs)                        //iterate over the pairs
+                    {
+                        String[] entry = pair.split(":");                   //split the pairs to get key and value
+                        map.put(entry[0].trim(), entry[1].trim());          //add them to the hashmap and trim whitespaces
+                    }
+*/
+
+                    getMvpView().onErrorToast(response.body().getMessage());
+                    getMvpView().hideLoading();
+
+                } else {
+                    String value = null;
+                    try {
+                        value = response.errorBody().string();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    value = value.substring(1, value.length() - 1);           //remove curly brackets
+                    String[] keyValuePairs = value.split(",");              //split the string to creat key-value pairs
+                    Map<String, String> map = new HashMap<>();
+
+                    for (String pair : keyValuePairs)                        //iterate over the pairs
+                    {
+                        String[] entry = pair.split(":");                   //split the pairs to get key and value
+                        map.put(entry[0].trim(), entry[1].trim());          //add them to the hashmap and trim whitespaces
+                    }
+
+
+                    getMvpView().onErrorToast(map.get("\"message\""));
+                    getMvpView().hideLoading();
+
                 }
-                value = value.substring(1, value.length() - 1);           //remove curly brackets
-                String[] keyValuePairs = value.split(",");              //split the string to creat key-value pairs
-                Map<String, String> map = new HashMap<>();
 
-                for (String pair : keyValuePairs)                        //iterate over the pairs
-                {
-                    String[] entry = pair.split(":");                   //split the pairs to get key and value
-                    map.put(entry[0].trim(), entry[1].trim());          //add them to the hashmap and trim whitespaces
-                }
-
-
-                getMvpView().onErrorToast(map.get("\"message\""));
-                getMvpView().hideLoading();
             }
 
             @Override
